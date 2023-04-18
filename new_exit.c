c3f083a578a174511d12febde05cb53d2dd450b2
HINKOKO <piotr62@live.fr>
2023-04-18 08:17:03 +0200

    revertt back the evil duck

--- new_exit.c
+++ new_exit.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-#include "math.h"
 
 /**
  *new_exit - function for exiting the program
@@ -11,21 +10,9 @@
 
 int new_exit(char *cmd, char **args, char **env)
 {
-	int exit_code = 0;
-
 	(void)cmd;
+	(void)args;
 	(void)env;
 
-
-	if (args[1])
-	/* there is an exit argument */
-	{
-		exit_code = atoi(args[1]);
-		if (exit_code == -1)
-		{
-			fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", args[1]);
-			exit(2);
-		}
-	}
-	exit(exit_code);
+	return (133);
 }
