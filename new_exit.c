275b658205e06057099cae1252cfc235c9fb9f92
HINKOKO <piotr62@live.fr>
2023-04-25 14:23:34 +0200

    surronding \if\ to check if exit is given with parameter, silly mistake

--- new_exit.c
+++ new_exit.c
@@ -15,26 +15,29 @@ int new_exit(char *cmd, char **args, char **env)
 	(void)cmd;
 	(void)env;
 
-	if (_isdigit(args[1]))
+	if (args[1] != NULL)
 	{
-		if (args[1])
+		if (_isdigit(args[1]))
 		{
-			check = atoi_error(args[1]);
-			if (check > 255)
-				check = check % 256;
-			if (check == -1)
-				return (1);
-			if (check < 0)
+			if (args[1])
 			{
-				fprintf(stderr, "./hsh: 1: exit: Illegal number: %d\n", check);
-				check = 2;
+				check = atoi_error(args[1]);
+				if (check > 255)
+					check = check % 256;
+				if (check == -1)
+					return (1);
+				if (check < 0)
+				{
+					fprintf(stderr, "./hsh: 1: exit: Illegal number: %d\n", check);
+					check = 2;
+				}
 			}
 		}
-	}
-	else
-	{
-		fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", args[1]);
-		check = 2;
+		else
+		{
+			fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", args[1]);
+			check = 2;
+		}
 	}
 	free_args(args);
 	return (check);
